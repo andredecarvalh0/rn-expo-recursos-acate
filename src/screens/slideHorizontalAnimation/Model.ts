86cad36c09089a42e0a1baaeefcce179420919b6
andre <andre.decarvalho1@gmail.com>
2022-09-10 10:47:12 -0300

    update

--- src/screens/slideHorizontalAnimation/Model.ts
+++ src/screens/slideHorizontalAnimation/Model.ts
@@ -4,29 +4,32 @@ export interface IProduct {
   color1: string;
   color2: string;
   picture: number;
+  ratio: number;
 }
-
 
 export const products = [
   {
     title: 'Hamburgue A',
-    subtitle: 'Hamburguer, Bacon, Salada',
+    subtitle: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     color1: '#F9AC8A',
     color2: '#FBC6AE',
-    picture: require('./assets/hamburguer1.png')    
+    picture: require('./assets/hamburguer1.png'),
+    ratio: 1    
   },
   {
     title: 'Hamburgue B',
-    subtitle: 'Hamburguer, Bacon, Salada',
+    subtitle: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     color1: '#4DD2A5',
     color2: '#63D8B0',
-    picture: require('./assets/hamburguer1.png')    
+    picture: require('./assets/hamburguer2.png'),
+    ratio: 1    
   },
   {
     title: 'Hamburgue C',
-    subtitle: 'Hamburguer, Bacon, Salada',
+    subtitle: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     color1: '#FEB829',
     color2: '#FDD446',
-    picture: require('./assets/hamburguer1.png')    
+    picture: require('./assets/hamburguer3.png'),
+    ratio: 757 / 735   
   },
 ]
--- src/screens/slideHorizontalAnimation/components/Cards.tsx
+++ src/screens/slideHorizontalAnimation/components/Cards.tsx
@@ -10,20 +10,20 @@ const { width } = Dimensions.get('window');
 
 export const cards = [
   {
-    picture: require('./cards/a.jpeg'),
-    caption: 'Um monte de comida e eu com fome :('
+    picture: require('../assets/cards/a.jpeg'),
+    caption: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
   },
   {
-    picture: require('./cards/b.jpg'),
-    caption: 'Um monte de comida e eu com fome :('
+    picture: require('../assets/cards/b.jpg'),
+    caption: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
   },
   {
-    picture: require('./cards/c.jpeg'),
-    caption: 'Um monte de comida e eu com fome :('
+    picture: require('../assets/cards/c.jpeg'),
+    caption: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
   },
   {
-    picture: require('./cards/d.jpeg'),
-    caption: 'Um monte de comida e eu com fome :('
+    picture: require('../assets/cards/d.jpeg'),
+    caption: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
   }
 ]
 
@@ -35,11 +35,11 @@ interface CardProps {
 const Card = ({ picture, caption }: CardProps) => {
   return (
     <>
-      <View>
-        <Image source={picture} />
+      <View style={styles.container}>
+        <Image source={picture}  style={styles.image}/>
       </View>
-      <View>
-        <Text>{caption}</Text>
+      <View style={styles.caption}>
+        <Text style={styles.text}>{caption}</Text>
       </View>
     </>
   )
@@ -48,9 +48,39 @@ const Card = ({ picture, caption }: CardProps) => {
 const Cards = () => {
   return (
     <View>
-      <Text>Cards</Text>
+      {cards.map( ({picture, caption}, index) => (
+        <Card key={index} picture={picture} caption={caption} />
+      ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 16,
+    marginHorizontal: 12,
+    height: width
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+  },
+  caption:{
+    marginHorizontal: 24,
+    padding: 24,
+    backgroundColor: '#fff',
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16,
+    marginBottom: 16
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#432406'
+  }
+})
 
 export default Cards;
